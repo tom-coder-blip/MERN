c9f28513f4a339c0097f5f2c53e9929e0315ebae
tom-coder-blip <vumatom10@gmail.com>
2025-07-03 09:46:17 +0200

    Update movies.js

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -6,10 +6,10 @@ import axios from "axios";
 
 class MovieDataService {
     getAll(page = 0) {
-        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
+        return axios.get(`https://mern-app-1hlo.onrender.com/api/v1/movies?page=${page}`)
     }
     get(id) {
-        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
+        return axios.get(`https://mern-app-1hlo.onrender.com/api/v1/movies/id/${id}`)
     }
     // find(title) {
     //     return axios.get(
@@ -18,23 +18,23 @@ class MovieDataService {
     // }
     find(query, by = "title", page = 0, rating) {
         return axios.get(
-            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
+            `https://mern-app-1hlo.onrender.com/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
         )
     }
     createReview(data) {
-        return axios.post("http://localhost:5000/api/v1/movies/review", data)
+        return axios.post("https://mern-app-1hlo.onrender.com/api/v1/movies/review", data)
     }
     updateReview(data) {
-        return axios.put("http://localhost:5000/api/v1/movies/review", data)
+        return axios.put("https://mern-app-1hlo.onrender.com/api/v1/movies/review", data)
     }
     deleteReview(id, userId) {
         return axios.delete(
-            "http://localhost:5000/api/v1/movies/review",
+            "https://mern-app-1hlo.onrender.com/api/v1/movies/review",
             { data: { review_id: id, user_id: userId } }
         )
     }
     getRatings() {
-        return axios.get("http://localhost:5000/api/v1/movies/ratings")
+        return axios.get("https://mern-app-1hlo.onrender.com/api/v1/movies/ratings")
     }
 }
 
